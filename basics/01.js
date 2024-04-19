7588dcba9b2d58b53fddedb95fbb2b53247751ad
Om Patil <ompatil220851@acropolis.in>
2024-04-19 13:05:56 +0530

    added new note of lecture 4

---
+++ basics/01.js
@@ -0,0 +1,22 @@
+// Video number 04 Code with chai-yt
+
+
+const accountId = 12343;
+let accountEmail = "ompatil@google.com";
+var accountPassw = "erwew";
+accountCity = "indore";
+let accountState ;
+
+//accountId = 2//not allowed
+
+accountEmail = "hfds.com";
+accountPassw = "212112";
+accountCity = "burhanpur";
+
+// new way to log in form of table
+console.table([accountEmail,accountId,accountPassw,accountCity,accountState]);
+
+
+/* Prefer not to use var 
+because if issue of block scope and functional scope
+ */
