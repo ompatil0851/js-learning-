9bb05b5eeb546d69f668b31e3c15b835dbf72e2c
Om Patil <ompatil220851@acropolis.in>
2024-04-20 12:27:57 +0530

    datatype and conversion of DT

---
+++ basics/02.js
@@ -0,0 +1,27 @@
+// video number 05 code with chai-yt
+/* Datatypes and ECMA standards */
+
+//alert(3+3) // we are using nodejd, not browser
+
+// code should be readability should be high
+
+let name = " Om ";
+let age = 20;
+let isLoggedIn = false;
+let state;
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true or false
+//null => standalone value 
+// undefined
+//symbol => To find unique
+
+//object
+
+console.log(typeof undefined);// undefined
+console.log(typeof null); //object
+
+
+
---
+++ basics/03.js
@@ -0,0 +1,34 @@
+// video number 06 code with chai-yt
+
+//conversion and operations
+
+let score = undefined;
+// console.log(typeof(score));//number 
+
+let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));// string
+
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33abc "=> NaN
+// true => 1; false => 0
+
+let isLoggedIn = "om";
+
+let booleanIsLoggedIn =  Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 = true ; 0 = false
+//" " => false
+// "Om"=> true
+
+
+let someNumber =  22;
+
+let stringNumber = String(someNumber); //string conversion
+console.log(typeof(stringNumber)); //string
+
+
+
+
