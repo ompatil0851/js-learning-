73fab965c41ad104d1fdd6158d83ed571ad15f23
Om Patil <ompatil220851@acropolis.in>
2024-04-20 12:46:06 +0530

    increament operator

--- basics/03.js
+++ basics/03.js
@@ -30,5 +30,41 @@ let stringNumber = String(someNumber); //string conversion
 console.log(typeof(stringNumber)); //string
 
 
+// ******************Operations*****************
+
+
+let value =3;
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "hello "
+let str2 = "Om"
+
+let str3 = str1 + str2 
+console.log(str3);
+
+
+ console.log("1"+ 2) // 12
+ console.log(1 + "2") // 12
+ console.log("1"+ 2+2) // 122
+ console.log(1+2 +"2"); // 32
+
+let num1, num2 , num3;
+
+num1 = num3 = num3 =2 + 2;
+
+let gameCounter = 100 
+++gameCounter;
+console.log(gameCounter);
+// link to study 
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
+
+
+
+
+
 
 
