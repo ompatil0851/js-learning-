845673a612db4265b419204ff98203cbe190511b
Om Patil <ompatil220851@acropolis.in>
2024-04-20 12:56:14 +0530

    comparison

---
+++ basics/04.js
@@ -0,0 +1,27 @@
+// video number 8 code with chai-yt
+//Comparison of datatypes in javascript | chai aur #javascript
+
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2 != 1);
+
+
+console.log("2" > 1);// true
+console.log("02" > 1);// true
+
+console.log(null > 0);// false
+console.log(null == 0);// false
+console.log(null >= 0);// true
+
+console.log(undefined == 0); // false
+console.log(undefined < 0); // false 
+console.log(undefined > 0); // false
+
+// ===
+
+console.log("2" === 2);// false
+
+
+
